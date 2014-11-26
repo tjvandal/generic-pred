8963851998ee2d409b0bbc59fdcc396c1b59ee44
tjvandal <tvandal20@gmail.com>
2014-11-26 00:58:49 -0500

    adding some goodies

--- julia/logistic-example.jl
+++ julia/logistic-example.jl
@@ -3,28 +3,34 @@
 push!(LOAD_PATH, pwd())
 using Lyaponuv
 
-function logistic(x, mu)
+@everywhere function logistic(x, mu)
     x_next = mu * x * (1 - x) 
     return(x_next)
 end
 
-x = rand(1)[1]
-n = 10000
-mu = 4
-logistic_time_series = Array(Float64, n)
-for i=1:n
-    logistic_time_series[i] = logistic(x, mu)
-    x=logistic_time_series[i]
+@everywhere function main(x)
+	n = 100
+	mu = 4
+	logistic_time_series = Array(Float64, n)
+	for i=1:n
+	    logistic_time_series[i] = logistic(x, mu)
+	    x=logistic_time_series[i]
+	end
+
+	J = 1  ## reconstruction delay
+	m = 3  ## embedding dimens
+
+	next_x_values = 1
+	sample_size = 25
+	series = deepcopy(logistic_time_series)
+	for j=1:next_x_values
+	    val = lyaponuv_next(series, J, m, 10, sample_size)
+	    append!(series, [val])
+	end
+	return(0)
 end
 
-J = 1  ## reconstruction delay
-m = 3  ## embedding dimens
-
-next_x_values = 1
-sample_size = 5
-series = deepcopy(logistic_time_series)
-for j=1:next_x_values
-    val = lyaponuv_next(series, J, m, 10, sample_size)
-    append!(series, [val])
+for xi in rand(10)
+	println("Starting process")
+	r = main(xi)
 end
-
